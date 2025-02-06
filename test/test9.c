0bdfdd3c5e9b65aaf48507c434d0068a00ea5bbf
Rushikesh <rushikeshreddy4094@gmail.com>
2025-02-06 15:20:58 +0530

    Updates test case

--- test/test9.c
+++ test/test9.c
@@ -1,1 +1,25 @@
-int main() { int a = 5; int b = 10; a += b; return 0; }
+class MyClass {
+private:
+    int a;
+
+public:
+    MyClass() : a(0) {}
+    
+    void setA(int x) { a = x; }
+    int getA() const { return a; }
+
+    static void staticMethod() {}
+
+    virtual void print() { cout << "a: " << a << endl; }
+
+protected:
+    void setAProtected(int x) { a = x; }
+};
+
+int main() {
+    MyClass obj;
+    obj.setA(5);
+    obj.print();
+    MyClass::staticMethod();
+    return 0;
+}
